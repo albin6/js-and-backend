dec6cef16b5ff8214ca80ef2363924f8177ae2d0
Albin Aji <albinpariyarathu@gmail.com>
2025-05-24 19:53:05 +0530

    new file:   js/longest-common-prefix.js

---
+++ js/longest-common-prefix.js
@@ -0,0 +1,15 @@
+function findLongestCommonPrefix(words) {
+  const curr = words[0];
+  for (let i = 0; i < words[0].length; i++) {
+    let prefix = curr.slice(0, i + 1);
+    for (let j = 0; j < words.length; j++) {
+      if (!words[i].startsWith(prefix)) {
+        return prefix.slice(0, prefix.length - 1);
+      }
+    }
+  }
+}
+
+const words = ["flower", "flow", "flight"];
+
+console.log(findLongestCommonPrefix(words));
