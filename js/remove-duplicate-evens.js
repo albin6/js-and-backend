9d093cdb9b5f286f93ecb8d17b2ac2a54617fb4e
Albin Aji <albinpariyarathu@gmail.com>
2025-05-25 12:03:36 +0530

    modified:   js/remove-duplicate-evens.js

--- js/remove-duplicate-evens.js
+++ js/remove-duplicate-evens.js
@@ -1,3 +1,19 @@
-function removeDuplicateEven(nums) {}
+function removeDuplicateEven(nums) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 == 0 && map.has(nums[i])) {
+      for (let j = i; j < nums.length - 1; j++) {
+        nums[j] = nums[j + 1];
+      }
+      nums.length = nums.length - 1;
+      i--;
+    } else if (!map.has(nums[i])) {
+      map.set(nums[i], true);
+    }
+  }
+  return nums;
+}
 
-const nums = [4, 3, 1, 2, 4, 5, 2, 4, 1, 6, 6];
+const nums = [4, 3, 1, 1, 2, 4, 5, 2, 4, 1, 6, 6];
+
+console.log(removeDuplicateEven(nums));
