8521f5ae5e0545aaf089c2cf957bd7b77edc31f6
Albin Aji <albinpariyarathu@gmail.com>
2025-05-25 20:40:42 +0530

    modified:   js/test.js

--- js/test.js
+++ js/test.js
@@ -1,5 +1,47 @@
-async function greet() {
-  return "Hello";
-}
+// async function greet() {
+//   return "Hello";
+// }
 
-console.log(greet());
+// console.log(greet());
+
+// console.log([] == ![]);
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1000);
+// }
+
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+// const obj = {};
+// const key1 = { a: 1 };
+// const key2 = { b: 2 };
+
+// obj[key1] = "value1";
+// obj[key2] = "value2";
+
+// console.log(obj);
+
+// function outer() {
+//   let arr = [];
+//   for (var i = 0; i < 3; i++) {
+//     arr.push(function () {
+//       return i;
+//     });
+//   }
+//   return arr;
+// }
+
+// const funcs = outer();
+// console.log(funcs[0](), funcs[1](), funcs[2]());
+
+// const arr = [1, 2, , 4];
+// console.log(arr.length);
+// console.log(arr[2]);
+
+// console.log(5 < 6 < 7);
+// console.log(7 > 6 > 5);
+
+// const obj = Object.freeze({ name: "John" });
+// obj.name = "Jane";
+// console.log(obj.name);
