c7690dace572b5e68b9f66b04ef60ca372126148
Albin Aji <albinpariyarathu@gmail.com>
2025-05-26 11:35:02 +0530

    modified:   js/first-non-repeating.js

--- js/first-non-repeating.js
+++ js/first-non-repeating.js
@@ -1,17 +1,33 @@
+// O(n ^ 2) complexity
+// function firstUniqueChar(str) {
+//   let flag;
+//   for (let i = 0; i < str.length; i++) {
+//     flag = false;
+//     for (let j = 0; j < str.length; j++) {
+//       if (i !== j && str[i] == str[j]) {
+//         flag = true;
+//         break;
+//       }
+//     }
+//     if (!flag) {
+//       return str[i];
+//     }
+//   }
+//   return false;
+// }
+
 function firstUniqueChar(str) {
-  let flag;
-  for (let i = 0; i < str.length; i++) {
-    flag = false;
-    for (let j = 0; j < str.length; j++) {
-      if (i !== j && str[i] == str[j]) {
-        flag = true;
-        break;
-      }
-    }
-    if (!flag) {
-      return str[i];
+  const freq = {};
+  for (const char of str) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+
+  for (const char of str) {
+    if (freq[char] === 1) {
+      return char;
     }
   }
+
   return false;
 }
 
