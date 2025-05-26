0940b61346cfb73ba5e3d7e6310cfdd86205c116
Albin Aji <albinpariyarathu@gmail.com>
2025-05-26 13:01:57 +0530

    new file:   js/remove-character-from-string.js

---
+++ js/remove-character-from-string.js
@@ -0,0 +1,7 @@
+function removeChar(str, num) {
+  //   return str.slice(0, num) + str.slice(num + 1);
+  //   return str.replace("l", "");
+  //   return str.replaceAll("l", "");
+}
+
+console.log(removeChar("hello", 1));
