d3218f7b5d3591e84cfc680cba72695173073024
Albin Aji <albinpariyarathu@gmail.com>
2025-05-27 07:40:29 +0530

    new file:   js/strict-mode.js

---
+++ js/strict-mode.js
@@ -0,0 +1,16 @@
+// "use strict";
+
+// function show() {
+//   console.log(this); // undefined (not `window`)
+// }
+
+// show();
+
+// Object.defineProperty(globalThis, "PI", { value: 3.14, writable: false });
+// PI = 3.14159;
+
+// function something(a, a) {
+//   console.log(a);
+// }
+
+// something(10, 20);
